be8a9ad368b1fa427e212f77545ed5bc525e531c
skyzhou0 <hao.s.zhou@outlook.com>
2021-10-10 15:47:30 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # expres-server-nodejs
+
+In this project, we will be looking into how we can use express framework to build a REST API. 
+
+## Project Set up
+To initialize the project
+```bash
+# update npm
+sudo npm install -g npm
+# To create package.json file, let's run
+npm init
+# install express module.
+npm install express
+# check the version and dependecies 
+npm list 
+# or
+npm list --deepth=0
+```
+
+# Develope the RESTful API
+```bash
+node index.js
+
+# test
+curl http://localhost:3000
+curl http://localhost:3000/api/courses
+
+# install nodemon
+sudo npm install -g nodemon
+nodemon index.js
+
+# set the port number for the API server. 
+const port = process.env.PORT || 3000;
+# run
+export PORT=5000 
+# then the port number will be at 5000 instead of the default 3000.
+
+
+```
+
+## Input Validation
+```bash
+npm install joi
+```
+Not that joi.validate() has been deprecated after version 16. To validate a schema object,
+we should use joi object.
+```javascript
+const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+const result = schema.validate(req.body);
+```
---
+++ index.js
@@ -0,0 +1,110 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+// Allow the app to use express json input.
+app.use(express.json());
+
+// Simulated database array
+const courses = [
+    { id: 1, name: "course1"},
+    { id: 2, name: "course2"},
+    { id: 3, name: "course3"},
+];
+
+app.get('/', (req, res) => {
+    res.send({"message": "hello nodemon"});
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+// Post request
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+// Validated Input Post request
+app.post('/api/courses_validate', (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const result = schema.validate(req.body);
+    console.log(result);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message)
+    };
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+
+app.put('/api/courses/:id', (req, res) => {
+    //look for the course, if not exist, return 404.
+    // const id = parseInt(req.params.id);
+    const course = courses.find( c => c.id == parseInt(req.params.id));
+    if (!course)  {
+        res.status(404).send("couse not found!");
+        return;
+    };
+    
+    // Validate if invalid, return 400 - bad request.
+    const { error } = inputSchemaValidation(req.body);
+    console.log(error);
+
+    if (error) {
+        res.status(400).send(error.details[0].message)
+    };
+
+    // Update course, return the updated course
+    course.name = req.body.name;
+    res.send(course);
+
+})
+
+// Route parameters
+app.get('/api/posts/:year/:month', (req, res) => {
+    // res.send(req.params.year);
+    res.send(req.params);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find( c => c.id == parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send("couse not found!");
+        return;
+    };
+    res.send(course);
+});
+
+
+
+const port = process.env.PORT || 3000;
+
+// app.listen(3000, () => console.log('Listen on port 3000 ....'));
+app.listen(port, () => console.log(`Listen on port ${port} ....`));
+
+
+function inputSchemaValidation(courseName) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return result = schema.validate(courseName);
+    // console.log(result);
+};
